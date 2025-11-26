774fbff2ff98fb4d2cacb599b5df3c8169896f98
sarah-alewijnse <sarah.alewijnse@cefas.gov.uk>
2025-11-26 16:52:30 +0000

    Fixed IUCN and CITES listings

--- code/data-prep/05-iucn-cites.R
+++ code/data-prep/05-iucn-cites.R
@@ -7,6 +7,7 @@ library(readr) # Better file reading
 library(data.table) # Better data wrangling
 library(dplyr) # Use if you have to
 library(tidyr) # Column separation
+library(beepr) # Beep when done
 # Taxnonomic packages
 library(worrms)
 library(rgbif)
@@ -17,7 +18,8 @@ library(rcites)
 # Get rid of scientific notation
 options(scipen = 9999)
 
-# Remember to set access tokens
+# Load access tokens
+source(here::here(".Renviron"))
 
 # Load data --------------------------------------------------------------------
 
@@ -59,73 +61,70 @@ import_data_gbif <- merge.data.table(import_data, gbif_taxonomy,
                                      by.x = c("Binomial", "Genus", "family", "order"),
                                      by.y = c("verbatim_name", "verbatim_genus", "verbatim_family", "verbatim_order"),
                                      suffixes = c("_worms", "_gbif"),
-                                     all.x = TRUE)
+                                     all.x = TRUE) %>% data.table()
 
 # Get IUCN listing -------------------------------------------------------------
 
-get_listings <- import_data_gbif$canonicalName %>% unique()
+# Get unique listings
+get_listings <- import_data_gbif[, .(Binomial, 
+                                     rank_gbif)] %>% unique()
+
+# Separate genus and species
+get_listings <- separate(get_listings,
+                         col = Binomial,
+                         into = c("Genus", "Species"),
+                         sep = c(" "),
+                         remove = F) %>% data.table()
+
+# Set column for IUCN listing
+get_listings[, IUCN_listing := NA]
 
 # Create progress bar
-pb <- progress_bar$new(total = length(get_listings))
 # There are lots of try states here, otherwise if the API can't find something R 
 # throws an error and ends the loop
-iucn_listings <- data.frame()
-for(i in 1:length(get_listings)){
+for(i in 1:nrow(get_listings)){
+  listing <- get_listings[i, ]
   # Try retrieving rredlist result
-  iucn <- try(rredlist::rl_search(name = as.character(get_listings[i])))
-  iucn <- try(iucn[["result"]])
-  iucn_listings <- try(rbind(iucn_listings, iucn))
-  pb$tick()
+  if(listing$rank_gbif == "SPECIES"){
+    iucn <- try(rredlist::rl_species(genus = listing$Genus, species = listing$Species,
+                                     key = KEY))
+    if(class(iucn) != "try-error"){
+      iucn <- try(iucn[["assessments"]])
+      iucn <- try(unique(iucn$red_list_category_code))
+      get_listings$IUCN_listing[i] <- iucn
+    }
+  }
+  iucn <- NULL
   print(i)
 };beep()
 
-# Select fields of interest
-iucn_listings_tidy <- iucn_listings[, c("scientific_name",
-                                        "category",
-                                        "criteria",
-                                        "population_trend")] %>% unique()
-
-colnames(iucn_listings_tidy)[2] <- "iucn_listing"
 
 # Search for CITES listing -----------------------------------------------------
 
-# Create progress bar
-pb <- progress_bar$new(total = length(get_listings))
-# Loop over 
-cites_listings <- data.frame(listing = get_listings,
-                             cites = NA) %>% data.table()
-cites_listings$cites <- as.character(cites_listings$cites)
-for(i in 1:nrow(cites_listings)){
-  cites <- try(rcites::spp_taxonconcept(query_taxon = cites_listings$listing[i],
-                                        verbose = F))
-  if(class(cites) == "spp_taxon"){
-    cites <- cites[["general"]]
-    cites_result <- cites$cites_listing
-    cites_listings[i, cites := cites_result]
-  }
-  pb$tick()
-  print(i)
-}
+# Set column for CITES listing
+get_listings[, CITES_listing := NA]
 
-# Join to data -----------------------------------------------------------------
+# Load cites listings data
+# from https://checklist.cites.org/#/en
+cites_listings <- read.csv(here::here("data",
+                                      "original-data",
+                                      "Index_of_CITES_Species_2025-11-26 16_41.csv")) %>%
+  data.table()
 
-# Combine with previous listings
-listings_iucn_cites <- merge.data.frame(iucn_listings_tidy,
-                                        cites_listings,
-                                        by.x = "scientific_name",
-                                        by.y = "listing",
-                                        all = TRUE) %>% unique()
+# Select columns
+cites_listings <- cites_listings[, .(FullName, Genus, Species, RankName, CurrentListing)]
 
-
-# Check for duplicates
-listings_dupes <- rbind(dplyr::filter(listings_iucn_cites, duplicated(scientific_name)),
-                        dplyr::filter(listings_iucn_cites, duplicated(scientific_name, fromLast = TRUE)))
+# join to listings
+get_listings <- merge.data.frame(get_listings,
+                                 cites_listings,
+                                 by.x = c("Binomial", "Genus", "Species", "rank_gbif"),
+                                 by.y = c("FullName", "Genus", "Species", "RankName"),
+                                 all.x = TRUE)
 
 # Join to data
 data_iucn_cites <- data.table::merge.data.table(import_data_gbif,
-                                                listings_iucn_cites,
-                                                by.x = "Binomial",
-                                                by.y = "scientific_name",
+                                                get_listings,
+                                                by = "Binomial",
                                                 all.x = TRUE) %>% data.table()
 
 # Check and correct listings ---------------------------------------------------
