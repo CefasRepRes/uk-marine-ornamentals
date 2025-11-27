302b4e698bb70fc8b3d9a214a86b03e66c299935
sarah-alewijnse <sarah.alewijnse@cefas.gov.uk>
2025-11-27 09:43:53 +0000

    Sped up code

--- code/data-prep/01-currency-conversion.R
+++ code/data-prep/01-currency-conversion.R
@@ -6,13 +6,11 @@ library(here) # Easy filepaths
 library(readr) # Better file reading
 library(data.table) # Better data wrangling
 library(dplyr) # Use if you have to
-library(progress) # Progress bar
 
 # Load data --------------------------------------------------------------------
 
 import_data <- readr::read_csv(here::here("data",
                                           "original-data",
-                                          "invoice-data",
                                           "import-data.csv"),
                                # Ensure correct column types
                                col_types = cols(Import_Date = col_date(format = "%Y-%m-%d"),
@@ -53,54 +51,40 @@ unique(import_data$Currency)
 
 ## Convert to USD ==============================================================
 
-# Create new column
-import_data$Value_USD <- NA
-
-# Create progress bar
-pb <- progress_bar$new(total = nrow(import_data))
-
-# Loop over the data set, checking the currency and applying the appropriate conversion
 # Conversions from XE on 10/09/2019 (modal import date)
-for(i in 1:nrow(import_data)){
-  import_data$Value_USD[i] <- with(import_data[i, ],
-                                   if(is.na(Currency)){
-                                     NA
-                                     # USD stays the same
-                                   } else if(Currency == "USD"){
-                                     Total_Cost
-                                     # Singapore dollar (SGD)
-                                   } else if(Currency == "SGD"){
-                                     Total_Cost * 0.7250184458
-                                     # Pound sterling (GBP)
-                                   } else if(Currency == "GBP"){
-                                     Total_Cost * 1.2354212247
-                                     # Indonesian rupiah (IDR)
-                                   } else if(Currency == "IDR"){
-                                     Total_Cost * 0.0000711798
-                                     # Japanese yen (JPY)
-                                   } else if(Currency == "JPY"){
-                                     Total_Cost * 0.0093172594
-                                     # Malaysian ringgit
-                                   } else if(Currency == "MYR"){
-                                     Total_Cost * 0.2400292384
-                                     # Euro (EUR)
-                                   } else if(Currency == "EUR"){
-                                     Total_Cost * 1.1044777642
-                                     # Australian dollar (AUD)
-                                   } else if(Currency == "AUD"){
-                                     Total_Cost * 0.6865519982
-                                     # Kenyan shilling (KES)
-                                   } else if(Currency == "KES"){
-                                     Total_Cost * 0.0096490690
-                                     # NA if no currency
-                                   } else if(Currency == "CAN"){
-                                     # Canadian dollar (CAN)
-                                     Total_Cost * 0.7609645552
-                                   } else {
-                                     NA
-                                   })
-  pb$tick()
-}
+
+# Add column
+import_data[, Value_USD := as.numeric(NA)]
+
+# USD
+import_data[Currency == "USD", Value_USD := Total_Cost]
+
+# SGD
+import_data[Currency == "SGD", Value_USD := Total_Cost * 0.7250184458]
+
+# GBP
+import_data[Currency == "GBP", Value_USD := Total_Cost * 1.2354212247]
+
+# IDR
+import_data[Currency == "IDR", Value_USD := Total_Cost * 0.0000711798]
+
+# JPY
+import_data[Currency == "JPY", Value_USD := Total_Cost * 0.0093172594]
+
+# MYR
+import_data[Currency == "MYR", Value_USD := Total_Cost * 0.2400292384]
+
+# EUR
+import_data[Currency == "EUR", Value_USD := Total_Cost * 1.1044777642]
+
+# AUD
+import_data[Currency == "AUD", Value_USD := Total_Cost * 0.6865519982]
+
+# KES
+import_data[Currency == "KES", Value_USD := Total_Cost * 0.0096490690]
+
+# CAN
+import_data[Currency == "CAN", Value_USD := Total_Cost * 0.7609645552]
 
 # Check currencies again
 unique(import_data$Currency)
@@ -115,5 +99,5 @@ data.table::fwrite(import_data, here::here("data",
                                            "modified-data",
                                            "invoice-data",
                                            "01-import-data-currency.csv"),
-                   row.names = F,
+                   row.names = FALSE,
                    na = NA)
